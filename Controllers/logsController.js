7b6c0b2a8dc43da07ffabe4a9aebdd13b9f6c3c5
SHUBHAM VERMA <shubhamverma2604@gmail.com>
2024-04-06 22:59:11 +0530

    removed consoles

--- Controllers/logsController.js
+++ Controllers/logsController.js
@@ -11,51 +11,11 @@ const createUserLog = async (userId) => {
     res.status(500).send("Server Error");
   }
 };
-
-// const getDauAndMau = async (req, res) => {
-//   try {
-//     const { time } = req.body;
-
-//     let startTime, endTime;
-
-//     switch (time) {
-//       case "last_30_minutes":
-//         endTime = new Date();
-//         startTime = new Date(endTime - 30 * 60 * 1000);
-//         break;
-//       case "today":
-//         endTime = new Date();
-//         endTime.setHours(23, 59, 59, 999); // End of the current day
-//         startTime = new Date(endTime);
-//         startTime.setHours(0, 0, 0, 0); // Start of the current day
-//         break;
-//       case "daily":
-//         endTime = new Date();
-//         startTime = new Date(endTime - 30 * 24 * 60 * 60 * 1000); // Last 30 days
-//         break;
-//       case "monthly":
-//         endTime = new Date();
-//         endTime.setMonth(endTime.getMonth() + 1); // Move to next month
-//         endTime.setDate(0); // Last day of the current month
-//         startTime = new Date(endTime);
-//         startTime.setMonth(startTime.getMonth() - 12); // 12 months ago
-//         startTime.setDate(1); // First day of the month
-//         break;
-//       default:
-//         return res.status(400).json({ message: "Invalid time range" });
-//     }
-
-//     const count = await logService.getUniqueUsersCount({ startTime, endTime });
-
-//     return res.status(200).send(`No of active user in ${time} are ${count}`);
-//   } catch (error) {
-//     return res.status(500).json({ message: error.message });
-//   }
-// };
+ 
 
 const getDauAndMau = async (req, res) => {
   try {
-    console.log("TIME========>", req.query);
+    // console.log("TIME========>", req.query);
 
     const { time } = req.query;
 
